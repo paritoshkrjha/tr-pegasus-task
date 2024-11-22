2508f83dd899650b7d627a43cb26e5beb8c4c7a9
Kulsrestha Joshi <kjkulsrestha2002@gmail.com>
2024-11-22 12:30:14 +0530

    add auth service

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule, NoopAnimationsModule, provideAnimations, provideNoopAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(),BrowserAnimationsModule,NoopAnimationsModule,provideNoopAnimations(),provideAnimations()]
 };
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' }, 
     { path: 'login', component: LoginComponent }, 
+    { path: 'home', component: HomeComponent }, 
+
 ];
